884b6531d9c57c2b2098a30a7c1ba0751fa072a6
krishna <krishna.wattamwar@graymatrix.com>
2019-06-26 17:42:40 +0530

    rm file changes

--- dashboard_script/disk_space.sh
+++ dashboard_script/disk_space.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+FILE_PATH="/opt/network_info/"
 df -h / |  awk '{ if(NR==2) print $5 }' | cut -c 1,2 | while read output;
 do
-if [ $output -gt 85 ];then
-echo "\n$(hostname -I | awk '{print $1}') Server is Running out of space \"$partition ($output%)\" on$(date +" %d/%m/%Y at %R")" > /opt/network_info/disk_space.txt
-else
-rm /opt/network_info/disk_space.txt
+if [ $output -gt 84 ];then
+echo "$(hostname -I | awk '{print $1}') Server is Running out of space \"$partition ($output%)\" on$(date +" %d/%m/%Y at %R")" > /opt/network_info/disk_space.txt
 fi
+
+if [ $output -lt 84 ];then
+rm $FILE_PATH/disk_space.txt
+fi
+
 done
+

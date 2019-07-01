e0a1ef2242eaf8e4a36270ff50af6d543fe4a2bb
krishna <krishna.wattamwar@graymatrix.com>
2019-07-01 10:40:25 +0530

    server up script changes

--- up_server.sh
+++ up_server.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 NOTIFYEMAIL="kalpesh.shedge@graymatrix.com,faiyaz.siddiqui@graymatrix.com,itsupport@graymatrix.com"
+NOTIFYEMAIL1="krishna.wattamwar@graymatrix.com"
 
-pm2 status | grep "online" | wc -l | while read output;
-do
-if [ $output -eq 3 ]; then
-echo "Hello Support_Team, \n\n$(hostname -I | awk '{print $1}') Server is up on $(date +" %d/%m/%Y at %R") \n\n\nRegards, \nTeam ITSupport"  | mail -s "$(hostname -I | awk '{print $1}') server is up now" $NOTIFYEMAIL
+#pm2 status | grep "online" | wc -l | while read output;
+#do
+#if [ $output -eq 3 ]; then
+echo "Hello Support_Team, \n\n$(hostname -I | awk '{print $1}') Server is up on $(date +" %d/%m/%Y at %R") \n\n\nRegards, \nTeam ITSupport"  | mail -s "$(hostname -I | awk '{print $1}') server is up now" $NOTIFYEMAIL1
 
-else
-echo "Hello Support_Team, \n\npm2 services is down on $(hostname -I | awk '{print $1}') server at $(date +" %d/%m/%Y at %R"). \nPlease check the issue and Reslove it as soon as possible. \n\n\nRegards, \nTeam ItSupport"  | mail -s "pm2 service is down after rebooting the $(hostname -I | awk '{print $1}') server" $NOTIFYEMAIL
-fi
-done
+#else
+#echo "Hello Support_Team, \n\npm2 services is down on $(hostname -I | awk '{print $1}') server at $(date +" %d/%m/%Y at %R"). \nPlease check the issue and Reslove it as soon as possible. \n\n\nRegards, \nTeam ItSupport"  | mail -s "pm2 service is down after rebooting the $(hostname -I | awk '{print $1}') server" $NOTIFYEMAIL
+#fi
+#done

61ef54a04295ce3328fe465a0d097df371ecd6fe
krishna <krishna.wattamwar@graymatrix.com>
2019-07-11 10:39:45 +0530

    editing server up script for each server

---
+++ server_up/server_up_21.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NOTIFYEMAIL="kalpesh.shedge@graymatrix.com,faiyaz.siddiqui@graymatrix.com,itsupport@graymatrix.com"
+#NOTIFYEMAIL1="krishna.wattamwar@graymatrix.com"
+
+echo -e "Hello Support_Team, \n\n192.168.0.21 Server is up on $(date +" %d/%m/%Y at %R") \n\n\nRegards, \nTeam ITSupport"   | mail -s "192.168.0.21 server is up now" $NOTIFYEMAIL
+

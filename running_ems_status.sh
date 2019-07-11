41a517b65cfaeaddd98e4de402694d814b4833a9
krishna <krishna.wattamwar@graymatrix.com>
2019-07-11 12:58:04 +0530

    adding new script running ems status aft restarted the server

---
+++ running_ems_status.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 30m
+NOTIFYMAIL="itsupport@graymatrix.com,kalpesh.shedge@graymatrix.com,faiyaz.siddiqui@graymatrix.com"
+ADBPATH="/opt/genymobile/genymotion/tools/"
+running_ems=$(${ADBPATH}./adb devices -l | awk {'print $4'} | cut -c 7-50)
+ems_count=$(${ADBPATH}adb devices -l | grep "model" | wc -l )
+echo "Hello Support_Team, \n\nList of Running Bots on $(hostname -I | awk '{print $1}') are as below: \n-----------$running_ems
+============\nems_count=$ems_count\n============ \n\n\nRegards, \nITSupport"  | mail -s "List of running ems on $(hostname -I | awk '{print $1}')" $NOTIFYMAIL
+

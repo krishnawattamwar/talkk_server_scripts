51dbdb1ebe57b4bb6c704f4072b8bef1f69c3634
krishna <krishna.wattamwar@graymatrix.com>
2019-07-16 12:57:59 +0530

    ram alert script changes

--- ram_alert.sh
+++ ram_alert.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 NOTIFYMAIL="itsupport@graymatrix.com,kalpesh.shedge@graymatrix.com,faiyaz.siddiqui@graymatrix.com"
+ram_space=$(echo "scale=2; $(vmstat -s  | head -n 5 | tail -n 1 | awk {'print $1'})/1024" | bc)
 vmstat -s  | head -n 5 | tail -n 1 | awk {'print $1'} | while read output;
 do
 #echo "$output"
 if [ $output -lt 150000 ]; then
-        echo "Hello Support_Team, \n\nRAM Space is $output KB on $(hostname -I | awk '{print $1}') server which is very low. Please check the server and Solve the RAM issue as soon as possible. \n\n\nRegards, \nTeam ItSupport"  | mail -s "RAM space is very low on $(hostname -I | awk '{print $1}') server" $NOTIFYMAIL
+        echo "Hello Support_Team, \n\nRAM Space is $ram_space MB on $(hostname -I | awk '{print $1}') server which is very low. Please check the server and Solve the RAM issue as soon as possible. \n\n\nRegards, \nTeam ItSupport" | mail -s "RAM space is very low on $(hostname -I | awk '{print $1}') server" $NOTIFYMAIL
 fi
 done
